0af0f0cd80c2d8519a40829c89d47c4e55c24d41
jahidcst <mdjahidhasancst1stshift@gmail.com>
2023-04-11 21:07:34 +0600

    All Question Done

--- src/component/Blogs.jsx
+++ src/component/Blogs.jsx
@@ -2,14 +2,28 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
+        <div >
             <h1 className='text-3xl text-center pt-5 font-bold text-violet-700'>Important Questions</h1>
-            <div className='p-12'>
-                <h2 className='text-xl font-semibold'> <span className='text-indigo-400'>Question-1:</span> When should we use Context API ?</h2>
-                <p><span>Answer: </span></p>
-                <h2 className='text-xl font-semibold'> <span className='text-indigo-400'>Question-2:</span></h2>
-                <h2 className='text-xl font-semibold'> <span className='text-indigo-400'>Question-3:</span></h2>
-                <h2 className='text-xl font-semibold'> <span className='text-indigo-400'>Question-4:</span></h2>
+            <div className='p-12 space-y-2'>
+                <h2 className='text-xl font-semibold'><span className='text-indigo-400'>Question-1:</span> When should we use Context API ?</h2>
+                <p className='pl-6'><span className='font-semibold text-rose-700'>
+                    Answer: </span> We should use Contex API  when some data needs to be accessible by many components at different nesting levels.
+                </p>
+
+                <h2 className='text-xl font-semibold'><span className='text-indigo-400'>Question-2:</span> What is Custom Hook in React ?</h2>
+                <p className='pl-6'><span className='font-semibold text-rose-700'>
+                    Answer: </span> Custom JavaScript hooks are reusable functions that a React JS software developer can use to add special and unique functionality to the React applications.
+                </p>
+
+                <h2 className='text-xl font-semibold'><span className='text-indigo-400'>Question-3:</span> What is useRef in  React ?</h2>
+                <p className='pl-6'><span className='font-semibold text-rose-700'>
+                    Answer: </span> useRef() is a built-in React hook that accepts one argument as the initial value and returns a reference. A reference is an object having a special property current.
+                </p>
+
+                <h2 className='text-xl font-semibold'><span className='text-indigo-400'>Question-4:</span>  What is useMemo in React ?</h2>
+                <p className='pl-6'><span className='font-semibold text-rose-700'>
+                    Answer: </span> useMemo() is a function that returns a memoized value of a passed in resource-intensive function.
+                    </p>
             </div>
         </div>
     );
