5c25e1d75b5cfc1b34e55855e4ea9033f6c052c8
jahidcst <mdjahidhasancst1stshift@gmail.com>
2023-04-12 11:23:19 +0600

    Some Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
+import React, { createContext } from "react";
+import Home from "./components/Home/Home";
+import { Outlet, useLoaderData, useNavigation } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 
-import React from 'react';
-import Header from './component/Header';
-import { Outlet } from 'react-router-dom';
-import Footer from './component/Footer';
+import Nav from "./components/Header/Nav";
+import Loading from "./components/Loading/Loading";
 
+export const JobDataContext = createContext([]);
 const App = () => {
+  const navigation = useNavigation();
+  if (navigation.state === "loading") {
+    return <Loading />;
+  }
+  const { jobs } = useLoaderData();
   return (
-    <>
-      <Header></Header>
-      <Outlet></Outlet>
-      <Footer></Footer>
-    </>
+    <JobDataContext.Provider value={jobs}>
+      <div>
+        <Nav />
+        <div className="min-h-[calc(100vh-152px)]">
+          <Outlet />
+        </div>
+        <Footer />
+      </div>
+    </JobDataContext.Provider>
   );
 };
 
